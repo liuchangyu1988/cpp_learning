a410b837802bcdee0d8010d32f2d6cb3a6f8281d
liuchangyu1988 <liuchangyu1988@gmail.com>
2024-12-14 11:51:35 -0500

    Update stdin_example.cc

--- stdin_example.cc
+++ stdin_example.cc
@@ -3,22 +3,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 int main() {
-    std::string line;
+	using namespace std;
+	std::string line;
 
-    std::cout << "Enter multiple lines of input (Ctrl+D to end):" << std::endl;
+	std::cout << "Enter multiple lines of input (Ctrl+D to end):" << std::endl;
 
-    // Read input until EOF (Ctrl+D on Linux/Mac, Ctrl+Z on Windows)
-    std::vector<std::string> input_list;
-    while (std::getline(std::cin, line)) {
-        std::cout << "You entered: " << line << std::endl;
-        input_list.emplace_back(line);
-    }
-    
-    for (const auto& line : input_list) {
-        std::cout << line << std::endl;
-    }
+	// Read input until EOF (Ctrl+D on Linux/Mac, Ctrl+Z on Windows)
+	std::vector<vector<std::string>> input_list;
+	while (std::getline(std::cin, line)) {
+		std::cout << "You entered: " << line << std::endl;
+		stringstream ss(line);
+		string word;
+		vector<string> word_list;
+		while(getline(ss, word, ' ')) {
+			cout << "word is " << word << endl;
+			word_list.emplace_back(word);
+		}
+		input_list.emplace_back(word_list);
+	}
 
-    return 0;
+	for (const auto& line : input_list) {
+		for (const auto& word : line) {
+			std::cout << word << ";";
+		}
+		cout << endl;
+	}
+
+	return 0;
 }
